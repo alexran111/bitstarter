6590192be8b060031da1b9ae7a9770c77f75a73e
Alex Ran <alexran111@gmail.com>
2013-07-12 23:45:54 +0000

    js bugs

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var fs = require('fs');
 
 var textFromFile=function(name) {
-  return fs.readFileSync(name).toString();   
-}
+  return fs.readFileSync(name);
+};
 
 app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(textFromFile("index.html");
+  response.send(textFromFile("index.html"));
 });
 
 var port = process.env.PORT || 5000;
